d7cecd6b2ae8dde2e01a398b4b6a4b85d1bf0e4b
rjprotiviti <jamesryan@nps.gov>
2015-10-25 11:29:51 -0400

    Update default.rb

--- src/cookbooks/metadata-aggregator/default.rb
+++ src/cookbooks/metadata-aggregator/default.rb
@@ -1,27 +1,19 @@
-directory '/root' do
- user 'root'
- group 'root'
- mode '0700'
-end
-
-if Dir.exists?('/etc/git/metadata-aggregator') then
- directory '/etc/git/HelloWorldChef' do
-  recursive true
-  action :delete
- end
-else
- if Dir.exists?('/etc/git') then
-  directory '/etc/git' do
-   owner 'root'
+  directory '/root' do
+   user 'root'
    group 'root'
-   mode '0755'
-   action :create
+   mode '0700'
   end
- end
- directory '/etc/git/metadata-aggregator' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
- end
-end
+
+   directory '/etc/repos/metadata-aggregator' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+   end
+
+  git '/etc/git/metadata-aggregator' do
+    repository 'https://metadata-aggregator:<token>@github.com/GSA/metadata-aggregator.git'
+    revision 'master'
+    user 'root'
+    action :sync
+  end
