c4c037b50ecb70b67aa9b936258f53b385485a54
rjprotiviti <jamesryan@nps.gov>
2015-10-25 11:33:26 -0400

    Update default.rb

--- src/cookbooks/metadata-aggregator/default.rb
+++ src/cookbooks/metadata-aggregator/default.rb
@@ -1,19 +1,19 @@
-  directory '/root' do
-   user 'root'
-   group 'root'
-   mode '0700'
-  end
+directory '/root' do
+ user 'root'
+ group 'root'
+ mode '0700'
+end
 
-   directory '/etc/repos/metadata-aggregator' do
-    owner 'root'
-    group 'root'
-    mode '0755'
-    action :create
-   end
+directory '/etc/repos/metadata-aggregator' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
 
-  git '/etc/git/metadata-aggregator' do
-    repository 'https://metadata-aggregator:<token>@github.com/GSA/metadata-aggregator.git'
-    revision 'master'
-    user 'root'
-    action :sync
-  end
+git '/etc/repos/metadata-aggregator' do
+  repository 'https://metadata-aggregator:<token>@github.com/GSA/metadata-aggregator.git'
+  revision 'master'
+  user 'root'
+  action :sync
+end
